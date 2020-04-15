f497d739d856564e40f1934cf4d90f2c75fdb2d4
Michele Azzolari <michele@azzolari.it>
2020-04-15 13:28:34 +0200

    Deck: store expansions while parsing cards

--- src/models/Deck.js
+++ src/models/Deck.js
@@ -4,13 +4,27 @@ import { arrayShuffle } from '../utils/arrayUtils';
 const _questions = [];
 const _answers = [];
 
+const _expansions = {};
+
 cards.forEach(card => {
+  if (!_expansions[card.expansion]) {
+    _expansions[card.expansion] = {
+      q: 0,
+      a: 0
+    };
+  }
   if (card.cardType === 'Q') {
     _questions.push(card);
+    _expansions[card.expansion].q++;
   } else {
     _answers.push(card);
+    _expansions[card.expansion].a++;
   }
 });
+
+export const getExpansions = function() {
+  return _expansions;
+};
 
 export const getDeck = function(expansions) {
   return {
